f6d3bbf5efc43923ce39528c43c9df93c0e72a02
Prathmesh Waghmare <prathmeshwaghmare289@gmail.com>
2026-02-17 12:14:31 +0530

    cleared old files

--- code/1523/src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import { getCurrentUser } from "../utils/auth";
-
-export default function ProtectedRoute({ children, redirectTo = "/login" }) {
-  const user = getCurrentUser();
-  if (!user) return <Navigate to={redirectTo} replace />;
-  return children;
-}
--- code/1617/README.md
+++
@@ -1,6 +0,0 @@
-# Voro React (Bootstrap version)
-
-This is a Vite + React port of your Bootstrap-based Voro jewellery site.
-To run:
-1. npm install
-2. npm run dev
